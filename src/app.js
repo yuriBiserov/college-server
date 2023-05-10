4c6488dfdda6db1ab92f9740abd86054fbbd30e2
yurabiserov93@gmail.com <your@email.example>
2023-05-10 17:10:44 +0300

    test

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-// const express = require('express')
-// const path = require('path')
-// require(path.join(__dirname, '../db/mongoose/connect.js'));
-// const StudentRouter = require('../routers/student_router.js')
-// const LecturerRouter = require('../routers/lecturer_router.js')
-// const CourseRouter = require('../routers/course_router.js')
-// const LessonRouter = require('../routers/lesson_router.js')
-// const ClassRouter = require('../routers/class_router.js')
+const express = require('express')
+const path = require('path')
+require(path.join(__dirname, '../db/mongoose/connect.js'));
+const StudentRouter = require('../routers/student_router.js')
+const LecturerRouter = require('../routers/lecturer_router.js')
+const CourseRouter = require('../routers/course_router.js')
+const LessonRouter = require('../routers/lesson_router.js')
+const ClassRouter = require('../routers/class_router.js')
 
-// const cors = require('cors');
+const cors = require('cors');
 
-// const app = express()
-// app.use(cors());
-// app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ limit: '50mb' }));
+const app = express()
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
-// app.use('/student', StudentRouter);
-// app.use('/lecturer', LecturerRouter);
-// app.use('/courses', CourseRouter);
-// app.use('/lessons', LessonRouter)
-// app.use('/classes', ClassRouter)
+app.use('/student', StudentRouter);
+app.use('/lecturer', LecturerRouter);
+app.use('/courses', CourseRouter);
+app.use('/lessons', LessonRouter)
+app.use('/classes', ClassRouter)
 
 
-// const port = process.env.PORT || 9000
-// app.listen(port, () => {
-//     console.log(`Listening at port: http://localhost:${port}`);
-// })
+const port = process.env.PORT || 9000
+app.listen(port, () => {
+    console.log(`Listening at port: http://localhost:${port}`);
+})
 
 
