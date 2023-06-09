f4d988fcb66775ff693e6d9b58e37bc8092c8d31
yurabiserov93@gmail.com <your@email.example>
2023-06-09 11:15:04 +0300

    mongo connection

--- db/mongoose/connect.js
+++ db/mongoose/connect.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
-async function connect() {
-    await mongoose.connect('mongodb://localhost:27017/College-App');
+const database = module.exports = () => {
+    const connectionParams = {
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    }
+    try{
+        mongoose.connect(
+            'mongodb+srv://yurabiserov93:SQboDGvDWCwfeTfK@cluster0.tsyl4ze.mongodb.net/college-collection?retryWrites=true&w=majority',
+            connectionParams
+            )
+        console.log("Database connected successfully")
+    }catch(err){
+        console.log(err)
+    }
 }
-connect();
+database()
+
+// async function connect() {
+//     await mongoose.connect('mongodb://localhost:27017/College-App');
+// }
+// connect();
 
 
 
